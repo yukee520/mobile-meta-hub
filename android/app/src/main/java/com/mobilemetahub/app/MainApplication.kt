447272e82fcefd01506982cb0041dd7d2687a85c
yukee520 <your-email@example.com>
2026-09-05 23:20:31 +0800

    fix: remove toReactHost method call from MainApplication

--- android/app/src/main/java/com/mobilemetahub/app/MainApplication.kt
+++ android/app/src/main/java/com/mobilemetahub/app/MainApplication.kt
@@ -3,12 +3,9 @@ package com.mobilemetahub.app
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -26,11 +23,8 @@ class MainApplication : Application(), ReactApplication {
         override val isHermesEnabled: Boolean = true
       }
 
-  override val reactHost: ReactHost
-    get() = DefaultReactNativeHost.toReactHost(applicationContext, reactNativeHost)
-
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+    SoLoader.init(this, false)
   }
 }
